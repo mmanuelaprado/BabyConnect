d76522b5aa79f1b9abd11840bfe3c306bcd77b20
mmanuelaprado <manuelalaccoletaexterna@gmail.com>
2025-12-15 16:35:35 -0300

    Delete components/layout.tsx

--- components/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet } from "react-router-dom";
-
-export default function Layout() {
-  return (
-    <div style={{ minHeight: "100vh", background: "#fafafa" }}>
-      <Outlet />
-    </div>
-  );
-}
