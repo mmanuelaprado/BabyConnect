228a9cd64f1577843564ad966ff03ac25cadb535
mmanuelaprado <manuelalaccoletaexterna@gmail.com>
2025-12-15 16:34:05 -0300

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,6 @@ import { Outlet } from "react-router-dom";
 export default function Layout() {
   return (
     <div style={{ minHeight: "100vh", background: "#fafafa" }}>
-      {/* Aqui depois você coloca header/menu */}
       <Outlet />
     </div>
   );
